c76e23d0aaeeff08731ea52bb0cb295a366b03f1
FlavioJunior2021 <flavinhoj78@gmail.com>
2023-12-24 16:15:37 -0300

    Core config: adjusted colors and icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        'purple-light': '#A881E6',
+        'purple': '#7450AC',
+        'purple-dark': '#523480',
+        'gray-100': '#FBF9FE',
+        'gray-200': '#AFABB6',
+        'gray-300': '#252529',
+        'gray-400': '#17171A',
+        'gray-500': '#111112',
+        'gray-600': '#0C0C0D',
+        'success-light': '#4E995E',
+        'success': '#2F723D',
+        'pink': '#DB5BBF',
+        'pink-dark': '#251622',
+        'orange': '#E07B67',
+        'orange-dark': '#261A17',
+        'yellow': '#BB9F3A',
+        'yellow-dark': '#211E12',
+        'green': '#8CAD51',
+        'green-dark': '#1C2015',
+        'blue': '#7B94CB',
+        'blue-dark': '#1A1D23',
+      }
     },
   },
   plugins: [],
